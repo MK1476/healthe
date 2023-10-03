33634a189cb089974c0b25f02f4401126a767fa5
MK1476 <99121asif@gmail.com>
2023-10-03 14:00:02 +0530

    host

--- new_life/new_life/settings.py
+++ new_life/new_life/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-e+pxd@+3#nf!wj(ta)x6y_al5-9a@nk(o1s(5j2z!u6oza@5+0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","54.198.105.70"]
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
